0604cf6b995786ea4b4873f9bc21716ab7af7944
Chris Johnson <cxjohnson@gmail.com>
2020-05-04 23:35:11 -0500

    Use php-mock/php-mock-phpunit and add some tests.

--- tests/CurlPostTest.php
+++ tests/CurlPostTest.php
@@ -5,18 +5,39 @@
  */
 namespace Cxj;
 
+use phpmock\phpunit\PHPMock;
 use PHPUnit\Framework\TestCase;
-
-function curl_exec($ch)
-{
-    return "Hi there, $ch";
-}
 
 class CurlPostTest extends TestCase
 {
-    public function testNamespace()
+    use PHPMock;
+
+    public function testCurl()
     {
-        $return = curl_exec("foo");
-        $this->assertEquals("Hi there, foo", $return);
+        $curl_exec = $this->getFunctionMock(__NAMESPACE__, "curl_exec");
+        $curl_exec->expects($this->once())->willReturn("body");
+
+        $ch = curl_init();
+        $this->assertEquals("body", curl_exec($ch));
+    }
+
+    public function testSuccess()
+    {
+        $curl_exec = $this->getFunctionMock(__NAMESPACE__, "curl_exec");
+        $curl_exec->expects($this->once())->willReturn("<return></return>");
+
+        $curl_getinfo = $this->getFunctionMock(__NAMESPACE__, "curl_getinfo");
+        $curl_getinfo->expects($this->once())->willReturn(200);
+
+        $test = new CurlPost("http://example.com");
+        $this->assertEquals("<return></return>", $test->sendAndReceive("XML"));
+    }
+
+    public function testTime()
+    {
+        $time = $this->getFunctionMock(__NAMESPACE__, "time");
+        $time->expects($this->once())->willReturn(3);
+
+        $this->assertEquals(3, time());
     }
 }
