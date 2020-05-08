2a91ce1d8f6cd5cb75c71390a4327391655333ac
Chris Johnson <cxjohnson@gmail.com>
2020-05-07 19:30:35 -0500

    Finish return data, handle errors, remove debug.

--- src/ValidateAddress.php
+++ src/ValidateAddress.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * @file ValidateAddress.php
- * Replace with one line description.
+ * Use USPS Web API to validate and standardize mailing addresses.
  */
 
 namespace Cxj;
 
+
+use RuntimeException;
 
 class ValidateAddress
 {
@@ -21,21 +22,35 @@ class ValidateAddress
         $this->parser = $parser;
     }
 
+    /**
+     * @param Address $input
+     *
+     * @return Address
+     * @throws RuntimeException
+     */
     public function validate(Address $input): Address
     {
         $response = $this->comm->sendAndReceive($input->getXml());
 
-        $this->parser->parse($response);
+        if (false === $this->parser->parse($response)) {
+            throw new RuntimeException("loadXML() failed");
+        }
 
-        var_dump($this->parser->getValue("Address1"));  // DEBUG
-        var_dump($this->parser->getValue("Address2"));  // DEBUG
-        var_dump($this->parser->getValue("City"));  // DEBUG
+        if (!empty($this->parser->getValue("Error"))) {
+            throw new RuntimeException(
+                "USPS returned: " . $this->parser->getValue("Description")
+            );
+        }
 
         return new Address(
             $this->parser->getValue("Address1"),
             $this->parser->getValue("Address2"),
-            $this->parser->getValue("City")
+            $this->parser->getValue("City"),
+            $this->parser->getValue("State"),
+            $this->parser->getValue("Zip5"),
+            $this->parser->getValue("Zip4"),
+            $this->parser->getValue("FirmName"),
+            $this->parser->getValue("Urbanization")
         );
     }
-
 }
